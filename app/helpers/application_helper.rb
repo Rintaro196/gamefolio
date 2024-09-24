d46e65ac6f83c4957526259dc3638040b1b7cad3
mana <nouri393914@gmail.com>
2024-09-24 20:31:23 +0900

    デフォルトのOGP画像を設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,27 +7,27 @@ module ApplicationHelper
   def default_meta_tags
     {
       site: 'GAMEFOLIO',
-      title: '都内スポーツ施設の横断検索サービス',
+      title: 'GAMEFOLIO',
       reverse: true,
       charset: 'utf-8',
-      description: 'Pluspoを使えば「スポーツ・日時・場所」の好きな組み合わせで自由にスポーツ施設を検索できます。',
-      keywords: 'スポーツ,スポーツ施設,東京',
+      description: 'ゲームを記録してポートフォリオを充実させよう',
+      keywords: 'ゲーム',
       canonical: request.original_url,
       separator: '|',
       og: {
-        site_name: :site,
-        title: :title,
-        description: :description,
+        site_name: 'GAMEFOLIO',
+        title: 'GAMEFOLIO',
+        description: 'ゲームを記録してポートフォリオを充実させよう',
         type: 'website',
         url: request.original_url,
-        image: image_url('ogp.png'), # 配置するパスやファイル名によって変更すること
+        image: image_url('ogp_image.png'),
         local: 'ja-JP'
       },
-      # Twitter用の設定を個別で設定する
+
       twitter: {
-        card: 'summary_large_image', # Twitterで表示する場合は大きいカードにする
-        site: '@', # アプリの公式Twitterアカウントがあれば、アカウント名を書く
-        image: image_url('ogp.png') # 配置するパスやファイル名によって変更すること
+        card: 'summary_large_image',
+        #site: '@', # アプリの公式Twitterアカウントがあれば
+        image: image_url('ogp_image.png')
       }
     }
   end
