57bf0406632f8d49a80981c580179b0a6f401c10
Mana_Lin <164577247+Rintaro196@users.noreply.github.com>
2024-09-26 02:18:53 +0000

    user_model add

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :name, :user_code, presence: true
+  validates :name, :age, length: { maximum: 255 }
+  validates :user_code, length] { ,maximum: 64 }
 end
