82b89afb856450da67106a2739b47b076e8b0164
mana <nouri393914@gmail.com>
2024-09-29 11:27:02 +0900

    メッセージ日本語化

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module Myapp
     # in config/environments, which are processed later.
     config.time_zone = "Asia/Tokyo"
     config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}").to_s]
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
