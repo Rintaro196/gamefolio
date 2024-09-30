180857099a1aaa1186d65b17dfc64a72f13e268c
Mana_Lin <164577247+Rintaro196@users.noreply.github.com>
2024-09-30 23:48:36 +0000

    users_contoroller add

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get "users/index"
+  get "users/show"
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
   root "home_pages#home"
+  resources :users, only: %i[index show]
 end
