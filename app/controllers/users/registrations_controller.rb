15815b7d3e916c87568599446f5221cafcb4b211
Mana_Lin <164577247+Rintaro196@users.noreply.github.com>
2024-10-01 00:33:12 +0000

    ユーザー詳細用のコントローラー追加

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -50,13 +50,17 @@ class Users::RegistrationsController < Devise::RegistrationsController
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :user_code, :age, :gender])
   end
 
+  def update_resource(resouece, params)
+    resource.update_without_password(params)
+  end
+
   # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def after_sign_up_path_for(resource)
+    user_path(id: current_user.id)
+  end
 
   # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def after_inactive_sign_up_path_for(resource)
+    user_path(id: current_user.id)
+  end
 end
