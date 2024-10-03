ae385f6a3614fb8e6f1c663669f5d413bea85ee6
Mana_Lin <164577247+Rintaro196@users.noreply.github.com>
2024-10-03 02:28:31 +0000

    sns_account_form add

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -42,12 +42,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [ :name, :user_code, :age, :gender ])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :name, :user_code ])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [ :name, :user_code, :age, :gender ])
+    devise_parameter_sanitizer.permit(:account_update, keys: [ :name, :user_code, :age, :gender, :profile, :twitter, :twitch, :discord ])
   end
 
   def update_resource(resouece, params)
