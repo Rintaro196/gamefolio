4bb6df8f2ba10a69d820007254f294d601173d59
Mana_Lin <164577247+Rintaro196@users.noreply.github.com>
2024-10-09 03:34:12 +0000

    controller write

--- app/controllers/search_games_controller.rb
+++ app/controllers/search_games_controller.rb
@@ -1,6 +1,34 @@
+require 'httparty'
+
 class SearchGamesController < ApplicationController
     before_action :authenticate_user!
 
     def search
+        @game_title = params[:title]
+    
+        if @game_title.present?
+          @game_info = search_game(@game_title)
+        else
+          @game_info = nil
+        end
+    end
+    
+    private
+
+    def search_game(title)
+        url = "https://api.igdb.com/v4/games"
+        headers = {
+          "Client-ID" => ENV['CLIENT_ID'],
+          "Authorization" => "Bearer #{ENV['ACCESS_TOKEN']}"
+        }
+    
+        query = <<-QUERY
+          fields name, genres.name, platforms.name, cover.url, release_dates.human, release_dates.region;
+          where name ~ "#{title}";
+        QUERY
+    
+        response = HTTParty.post(url, headers: headers, body: query)
+
+        JSON.parse(response.body)
     end
 end
--- app/forms/search_game_form.rb
+++ app/forms/search_game_form.rb
@@ -7,5 +7,17 @@ class SearchGameForm
     validates :game_title, presence: true, length: { maximum: 255 }
 
     def search_game(game_title)
+      client_id = ENV['CLIENT_ID']
+      access_token = ENV['ACCESS_TOKEN']
+
+      uri = "https://api.igdb.com/v4/games"
+
+      headers = {
+          "Client-ID" => "#{client_id}",
+          "Authorization" => "Bearer #{access_token}"
+      }
+
+      query = 'fields name, genres.name, release_dates.human; where name = "#{game_title}";'
+      @response = HTTParty.post(url, headers: headers, body: query)
     end
 end
