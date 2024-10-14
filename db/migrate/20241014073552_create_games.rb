94615d7d06f1301ad92a90717a7e511a18b01cfb
Mana_Lin <164577247+Rintaro196@users.noreply.github.com>
2024-10-15 08:54:01 +0900

    Update 20241014073552_create_games.rb
    gamesマイグレーションファイル中身編集

--- db/migrate/20241014073552_create_games.rb
+++ db/migrate/20241014073552_create_games.rb
@@ -1,6 +1,8 @@
 class CreateGames < ActiveRecord::Migration[7.2]
   def change
     create_table :games do |t|
+      t.string :game_title, null: false
+      t.string :cover_url
       t.timestamps
     end
   end
