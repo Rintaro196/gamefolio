0f395171b7a42ff92be7824efaf33dbc00fe885b
Mana_Lin <164577247+Rintaro196@users.noreply.github.com>
2024-10-15 09:54:04 +0900

    Update 20241014073540_create_platforms.rb

--- db/migrate/20241014073540_create_platforms.rb
+++ db/migrate/20241014073540_create_platforms.rb
@@ -2,6 +2,7 @@ class CreatePlatforms < ActiveRecord::Migration[7.2]
   def change
     create_table :platforms do |t|
       t.string :name, null: false
+      t.index :name, unique: true
       t.timestamps
     end
   end
