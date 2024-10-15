b2716536f0833733a6e9e735baa9a3c9823865da
Mana_Lin <164577247+Rintaro196@users.noreply.github.com>
2024-10-15 02:13:45 +0000

    user_games add

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_15_015429) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_15_015757) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -61,6 +61,17 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_15_015429) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "user_games", force: :cascade do |t|
+    t.integer "status", limit: 2, default: 0, null: false
+    t.bigint "user_id", null: false
+    t.bigint "game_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["game_id"], name: "index_user_games_on_game_id"
+    t.index ["user_id", "game_id"], name: "index_user_games_on_user_id_and_game_id", unique: true
+    t.index ["user_id"], name: "index_user_games_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "name", null: false
@@ -89,4 +100,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_15_015429) do
   add_foreign_key "game_genres", "genres"
   add_foreign_key "game_platforms", "games"
   add_foreign_key "game_platforms", "platforms"
+  add_foreign_key "user_games", "games"
+  add_foreign_key "user_games", "users"
 end
