be6c7a89b9512a67a4ff729c1f2a7dc76b8cf7e4
Mana_Lin <164577247+Rintaro196@users.noreply.github.com>
2024-10-15 10:03:52 +0900

    Update game.rb
    gamesのアソシエーション追記

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,8 @@
 class Game < ApplicationRecord
+    has_many :games_genres, dependent: :destroy
+    has_many :genres, thought: :games_genres
+    has_many :games_platforms, dependent: :destroy
+    has_many :platforms, thought: :games_platforms
+    
     validates :game_title, presence: true
 end
