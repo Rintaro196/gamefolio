d8d889d00e666405df552255a421b71c3c21882e
Mana_Lin <164577247+Rintaro196@users.noreply.github.com>
2024-10-15 09:54:14 +0900

    Update 20241014073502_create_genres.rb

--- db/migrate/20241014073502_create_genres.rb
+++ db/migrate/20241014073502_create_genres.rb
@@ -2,6 +2,7 @@ class CreateGenres < ActiveRecord::Migration[7.2]
   def change
     create_table :genres do |t|
       t.string :name, null: false
+      t.index :name, unique: true
       t.timestamps
     end
   end
