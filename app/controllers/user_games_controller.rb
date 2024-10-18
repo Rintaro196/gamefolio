7c5c43eff37121e831081587cc2283683daf776b
Mana_Lin <164577247+Rintaro196@users.noreply.github.com>
2024-10-18 00:16:43 +0000

    index_action add

--- app/controllers/user_games_controller.rb
+++ app/controllers/user_games_controller.rb
@@ -1,2 +1,8 @@
 class UserGamesController < ApplicationController
+    before_action :authenticate_user!
+
+    def index
+        user = User.find(params[:id])
+        @user_games = user.games
+    end
 end
