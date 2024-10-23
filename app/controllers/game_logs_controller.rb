52ba383ae9c65639d0ec5f04a289182204d85f48
Mana_Lin <164577247+Rintaro196@users.noreply.github.com>
2024-10-23 02:14:08 +0000

    game_log_controller write

--- app/controllers/game_logs_controller.rb
+++ app/controllers/game_logs_controller.rb
@@ -1,2 +1,32 @@
 class GameLogsController < ApplicationController
+    before_action :authenticate_user!
+    
+    def index
+        @game_logs = Game_log.includes(:user)
+    end
+
+    def show
+        @game_log = Game_log.includes(:user).find(params[:id])
+    end
+
+    def new
+        @game_log = Game_log.new
+    end
+
+    def create
+        @game_log = current_user.game_logs.build(game_log_params)
+        if @game_log.save
+            flash[:notice] = "ゲームログを記録しました"
+            redirect_to game_logs_path
+        else
+            flash[:alert] = "ゲームログを記録出来ませんでした"
+            render :new
+        end
+    end
+
+    private
+
+    def game_log_params
+        params.require(:game_log).permit(:title. :body, :play_time, :spending_amount).merge(user_game_id: params[:user_game_id])
+    end
 end
