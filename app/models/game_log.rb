441808cb85cfdb4e615bb7050645f98014dc2e97
Mana_Lin <164577247+Rintaro196@users.noreply.github.com>
2024-10-23 01:20:45 +0000

    model game_log write

--- app/models/game_log.rb
+++ app/models/game_log.rb
@@ -1,4 +1,8 @@
 class GameLog < ApplicationRecord
   belongs_to :user
   belongs_to :user_game
+
+  validates :title, length: { maximum: 140 }
+  validates :body, length: { maximum: 3000 }
+  validates :play_time, :spending_amount, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, allow_nil: true
 end
