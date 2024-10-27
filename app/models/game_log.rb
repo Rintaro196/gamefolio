1d7a246dea3ee1901898f4491d23f9eabfb07943
mana <nouri393914@gmail.com>
2024-10-27 13:45:38 +0900

    edit model of game_log to add images

--- app/models/game_log.rb
+++ app/models/game_log.rb
@@ -1,8 +1,10 @@
 class GameLog < ApplicationRecord
   belongs_to :user
   belongs_to :user_game
+  has_many_attached :images
 
   validates :title, presence: true, length: { maximum: 140 }
   validates :body, length: { maximum: 3000 }
   validates :play_time, :spending_amount, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, allow_nil: true
+  validates :images, attachment: { purge: true, content_type: %r{\Aimage/(png|jpeg|jpg)\Z} }
 end
