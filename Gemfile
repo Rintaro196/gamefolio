6568930cc3378c220045d90da1082e17a98b1892
mana <nouri393914@gmail.com>
2024-10-29 19:41:22 +0900

    fix form to upload images

--- Gemfile
+++ Gemfile
@@ -35,6 +35,7 @@ gem "bootsnap", require: false
 
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 gem "mini_magick"
+gem "activestorage-validator"
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- app/models/game_log.rb
+++ app/models/game_log.rb
@@ -6,5 +6,4 @@ class GameLog < ApplicationRecord
   validates :title, presence: true, length: { maximum: 140 }
   validates :body, length: { maximum: 3000 }
   validates :play_time, :spending_amount, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, allow_nil: true
-  validates :images, attachment: { purge: true, content_type: %r{\Aimage/(png|jpeg|jpg)\Z} }
 end
