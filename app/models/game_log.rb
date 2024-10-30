96a73a2bae968f747e61d4e5db027e505eb9d827
Mana_Lin <164577247+Rintaro196@users.noreply.github.com>
2024-10-30 05:11:20 +0000

    add validates_of_images

--- app/models/game_log.rb
+++ app/models/game_log.rb
@@ -6,4 +6,7 @@ class GameLog < ApplicationRecord
   validates :title, presence: true, length: { maximum: 140 }
   validates :body, length: { maximum: 3000 }
   validates :play_time, :spending_amount, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, allow_nil: true
+  validates :images, content_type: ['image/png', 'image/jpg', 'image/jpeg'],
+                     limit: { max: 4, message: "画像は４枚まで" },
+                     size: { less_than: 5.megabytes, message: "画像サイズが大きすぎます(5MBまで)" }
 end
