c93258db50b421ccd90dca5abe29921b7749dc0d
Mana_Lin <164577247+Rintaro196@users.noreply.github.com>
2024-10-30 02:04:10 +0000

    add method of resize

--- app/models/game_log.rb
+++ app/models/game_log.rb
@@ -6,4 +6,8 @@ class GameLog < ApplicationRecord
   validates :title, presence: true, length: { maximum: 140 }
   validates :body, length: { maximum: 3000 }
   validates :play_time, :spending_amount, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, allow_nil: true
+
+  def resized
+    return self.variant(resize: "100x100").processed
+  end
 end
