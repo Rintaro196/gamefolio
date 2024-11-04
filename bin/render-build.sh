8f631c880d2c1b771276f943bf2e1f1242e76be7
mana <nouri393914@gmail.com>
2024-11-04 10:20:39 +0900

    deploy to render

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,5 +3,6 @@ set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
+bundle exec rails db:reset
 bundle exec rails db:migrate
 bundle exec rake igdb_access_token:get_access_token
