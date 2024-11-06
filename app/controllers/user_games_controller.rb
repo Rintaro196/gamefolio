2cb65110849f8d78c9c1005888dda877fe7e126e
Mana_Lin <164577247+Rintaro196@users.noreply.github.com>
2024-11-06 01:24:25 +0000

    edit: user_gameアクション追加

--- app/controllers/user_games_controller.rb
+++ app/controllers/user_games_controller.rb
@@ -1,5 +1,6 @@
 class UserGamesController < ApplicationController
     before_action :authenticate_user!
+    before_action :set_user_game, only: %i[edit update destroy]
 
     def index
         @user = User.find(params[:id])
@@ -12,5 +13,29 @@ class UserGamesController < ApplicationController
         @total_spending_amount = @user_game.total_spnding_amount
 
         @game_logs = GameLog.includes(:user).where(user_game_id:  @user_game.id)
+    end
+
+    def edit; end
+
+    def update
+        if @user_game.update(user_game_params)
+            redirect_to game_log_path(@game_log), notice: "登録ゲームのステータスを更新しました"
+        else
+            flash.now[:alert] = "登録ゲームのステータスを更新出来ませんでした"
+            render :edit, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+    end
+
+    private
+
+    def set_user_game
+        @user_game = current_user.user_games.find(params[:id])
+    end
+
+    def user_game_params
+        params.require(:user_game).permit(:status)
     end
 end
