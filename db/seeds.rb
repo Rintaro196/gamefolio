2559b6d0bbbcbcca7d1a114bbcf8aa4bbe812878
Mana_Lin <164577247+Rintaro196@users.noreply.github.com>
2024-11-07 12:53:14 +0900

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 User.crate!(
   email: "sample@sample.com",
-  name: "Mana",
-  name_code: "Manalin014",
-  encrypted_password: "mana55b"
+  name: "sample",
+  name_code: "sample01",
+  encrypted_password: "samplesample"
   )
