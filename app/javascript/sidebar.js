e5d40a0b8e31020d94a163497270ee4cf2b51bca
Mana_Lin <164577247+Rintaro196@users.noreply.github.com>
2024-11-07 02:51:02 +0000

    fix: turbo対応

--- app/javascript/sidebar.js
+++ app/javascript/sidebar.js
@@ -1,23 +1,42 @@
-document.addEventListener('turbo:load', function() {
-    const toggleSidebar = document.getElementById('toggle-sidebar');
-    const closeSidebar = document.getElementById('close-sidebar');
-    const sidebar = document.getElementById('sidebar');
-    const sidebarLinks = document.querySelectorAll('.sidebar-link');
-  
-    toggleSidebar.addEventListener('click', function() {
+document.addEventListener('turbo:load', setupSidebar);
+document.addEventListener('turbo:render', setupSidebar);
+document.addEventListener('turbo:frame-load', setupSidebar);
+
+function setupSidebar() {
+  const toggleSidebar = document.getElementById('toggle-sidebar');
+  const closeSidebar = document.getElementById('close-sidebar');
+  const sidebar = document.getElementById('sidebar');
+  const sidebarLinks = document.querySelectorAll('.sidebar-link');
+
+  if (toggleSidebar && closeSidebar && sidebar) {
+    // 既存のイベントリスナーを削除する
+    toggleSidebar.replaceWith(toggleSidebar.cloneNode(true));
+    closeSidebar.replaceWith(closeSidebar.cloneNode(true));
+    sidebarLinks.forEach((link) => {
+      link.replaceWith(link.cloneNode(true));
+    });
+
+    // 更新した要素を再取得
+    const newToggleSidebar = document.getElementById('toggle-sidebar');
+    const newCloseSidebar = document.getElementById('close-sidebar');
+    const newSidebarLinks = document.querySelectorAll('.sidebar-link');
+
+    // 新しい要素にイベントリスナーを追加
+    newToggleSidebar.addEventListener('click', function() {
       sidebar.classList.toggle('-translate-x-full');
       sidebar.classList.toggle('translate-x-0');
     });
-  
-    closeSidebar.addEventListener('click', function() {
+
+    newCloseSidebar.addEventListener('click', function() {
       sidebar.classList.add('-translate-x-full');
       sidebar.classList.remove('translate-x-0');
     });
-  
-    sidebarLinks.forEach(function(link) {
+
+    newSidebarLinks.forEach(function(link) {
       link.addEventListener('click', function() {
         sidebar.classList.add('-translate-x-full');
         sidebar.classList.remove('translate-x-0');
       });
     });
-  });
+  }
+}
