f2260d79950971087d5a5c5c997e2b3f5bb65f84
mana <nouri393914@gmail.com>
2024-11-09 08:55:17 +0900

    delete: build_command

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,6 +4,3 @@ bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate
-<<COMMENTOUT
-bundle exec rake igdb_access_token:get_access_token
-COMMENTOUT
