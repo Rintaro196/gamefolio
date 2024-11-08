6b704e23ca6f17ba13bebd9885c8f824eacafbb8
Mana_Lin <164577247+Rintaro196@users.noreply.github.com>
2024-11-08 02:03:11 +0000

    rm: トークン取得タスク

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,4 +4,6 @@ bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate
+<<COMMENTOUT
 bundle exec rake igdb_access_token:get_access_token
+COMMENTOUT
