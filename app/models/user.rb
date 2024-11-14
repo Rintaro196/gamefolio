982cc8dbc71d9e0d66843378815356922285619e
Mana_Lin <164577247+Rintaro196@users.noreply.github.com>
2024-11-14 07:09:19 +0000

    update: ゲームログ登録時レベルアップ

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-  has_many :user_games
+  has_many :user_games, dependent: :destroy
   has_many :games, through: :user_games
-  has_many :game_logs
-  has_one_attached :user_icon
+  has_many :game_logs, dependent: :destroy
+  has_one_attached :user_icon, dependent: :destroy
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
@@ -16,7 +16,7 @@ class User < ApplicationRecord
 
   validates :user_icon, content_type: [ "image/png", "image/jpg", "image/jpeg" ],
                         size: { less_than: 5.megabytes, message: "画像サイズが大きすぎます(5MBまで)" }
-                        
+
   validates :level, presence: true, numericality: { only_integer: true, in: 1..9999 }
   validates :gem, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
