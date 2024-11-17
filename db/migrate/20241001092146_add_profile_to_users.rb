2384ee12bb5d6b1bec5fcfbdf602b1a0b4283268
mana <nouri393914@gmail.com>
2024-11-17 13:12:46 +0900

    edit: ユニーク制約解除

--- db/migrate/20241001092146_add_profile_to_users.rb
+++ db/migrate/20241001092146_add_profile_to_users.rb
@@ -1,12 +1,8 @@
 class AddProfileToUsers < ActiveRecord::Migration[7.2]
   def change
     add_column :users, :profile, :text
-    add_column :users, :twitter, :string, null: true
-    add_column :users, :twitch,  :string, null: true
-    add_column :users, :discord, :string, null: true
-
-    add_index :users, :twitter, unique: true, where: "twitter IS NOT NULL"
-    add_index :users, :twitch, unique: true, where: "twitch IS NOT NULL"
-    add_index :users, :discord, unique: true, where: "discord IS NOT NULL"
+    add_column :users, :twitter, :string
+    add_column :users, :twitch,  :string
+    add_column :users, :discord, :string
   end
 end
