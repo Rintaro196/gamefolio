cff04ad93ab1f2420c8357b0fa7984fefb6ced14
Mana_Lin <164577247+Rintaro196@users.noreply.github.com>
2024-11-21 02:44:57 +0000

    途中

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "バリデーションチェック" do
+
+    it "nullだとユーザー登録できないこと" do
+      user = user.new
+    end
+    it "user_codeバリデーション" do end
+    it "ageバリデーション" do end
+    it "profileバリデーション" do end
+    it "アカウントバリデーション" do end
+    it "user_iconバリデーション" do end
+  end
 end
