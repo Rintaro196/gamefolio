ddd2ae2e46c09c73eb0fc11769b5ba08a483b88e
Mana_Lin <164577247+Rintaro196@users.noreply.github.com>
2024-11-25 00:56:38 +0000

    add: create action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,26 @@
 class CommentsController < ApplicationController
+    before_action :authenticate_user!, only: %i[create edit destroy]
+
+    def create
+        comment = Comment.new(comment_params)
+        comment.user_id = current_user.id
+        if comment.save
+          flash[:notice] = "コメントしました"
+        else
+          flash[:alert] = "コメントできませんでした"
+        end
+        redirect_to request.referer || root_path
+    end
+
+    def edit
+    end
+
+    def destroy
+    end
+
+    private
+
+    def comment_patams
+      params.require(:comment).permit(:body).merge(game_log_id: params[:game_log_id])
+    end
 end
