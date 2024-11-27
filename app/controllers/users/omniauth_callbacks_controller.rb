4d965a71664cd3378461bf99c9f6d9a3cffb2ff9
mana <nouri393914@gmail.com>
2024-11-27 16:40:57 +0900

    add: リクエスト許可

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,7 +2,7 @@
 
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
-    @user = user.from_omniauth(request.env["omniauth.auth"])
+    @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
       flash[:notice] = "Google認証でログインしました"
