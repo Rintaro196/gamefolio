1d383115ce9abcedbfebfbdcfbe86e46ae89f331
Mana_Lin <164577247+Rintaro196@users.noreply.github.com>
2024-12-02 23:41:21 +0000

    add: notifications_controller

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,2 +1,14 @@
 class NotificationsController < ApplicationController
+    before_action :authenticate_user!
+
+    def index
+      @notifications = current_user.recieved_notifications.order(created_at: :desc).page(params[:page]).per(20)
+      @notifications.where(checked: false).each do |notification|
+        notification.update(checked: true)
+      end
+    end
+
+    def destory
+      @notifications = current_user.notifications.destroy_all
+    end
 end
