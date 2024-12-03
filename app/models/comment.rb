cacfe38cfdc376e8c087186e6288d94a7d20753b
Mana_Lin <164577247+Rintaro196@users.noreply.github.com>
2024-12-03 23:46:27 +0000

    edit: コールバック処理削除

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,17 +5,4 @@ class Comment < ApplicationRecord
   has_one :notification, as: :notifiable, dependent: :destroy
 
   validates :body, presence: true, length: { maximum: 300 }
-
-  after_create_commit :create_notification
-
-  private
-
-  def create_notification
-    return if self.user_id == self.game_log.user_id
-    Notification.create(
-      sender_id: self.user_id,
-      receiver_id: self.game_log.user_id,
-      notifiable: self
-    )
-  end
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -6,16 +6,4 @@ class Like < ApplicationRecord
 
   validates :user_id, uniqueness: { scope: :game_log_id }
 
-  after_create_commit :create_notification
-
-  private
-
-  def create_notification
-    return if self.user_id == self.game_log.user_id
-    Notification.create(
-      sender_id: self.user_id,
-      receiver_id: self.game_log.user_id,
-      notifiable: self
-    )
-  end
 end
