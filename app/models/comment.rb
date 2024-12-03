ddc0fc2574f4cdb3cb245ff6eb30e547838874cc
Mana_Lin <164577247+Rintaro196@users.noreply.github.com>
2024-12-03 00:08:43 +0000

    add: 通知作成処理

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,21 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :game_log
+  
+  has_one :notification, as: :notifiable, dependent: :destory
 
   validates :body, presence: true, length: { maximum: 300 }
+
+  after_create_commit :create_notification
+
+  private
+
+  def create_notification
+    return if self.user_id == self.game_log.user_id
+    Notification.create(
+      sender_id: self.user_id,
+      reciever_id: self.game_log.user_id,
+      notifiable: self
+    )
+  end
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -2,5 +2,21 @@ class Like < ApplicationRecord
   belongs_to :user
   belongs_to :game_log
 
+  has_one :notification, as: :notifiable, dependet: :destroy
+
   validates :user_id, uniqueness: { scope: :game_log_id }
+
+  after_create_commit :create_notification
+
+  private
+
+  def create_notification
+    return if self.user_id == self.game_log.user_id
+    Notification.create(
+      sender_id: self.user_id,
+      reciever_id: self.game_log.user_id,
+      notifiable: self
+    )
+  end
+
 end
