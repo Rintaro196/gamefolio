47f3e7b057ec576b6f649d5f9f5d824692814cef
mana <nouri393914@gmail.com>
2024-12-10 17:57:58 +0900

    edit: ログイン前のテスト

--- spec/system/game_logs_spec.rb
+++ spec/system/game_logs_spec.rb
@@ -1,9 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe "GameLogs", type: :system do
-  before do
-    driven_by(:rack_test)
+  let!(:user) { create(:user) }
+  let!(:game) { create(:game) }
+  let!(:user_game) { create(:user_game, user: user, game: game) }
+  let!(:game_log) { create(:game_log, user: user, user_game: user_game) }
+
+  describe "ログイン前" do
+
+    it "ゲーム一覧が見れる" do
+      visit game_logs_path
+      expect(page).to have_content(game_log.title)
+      expect(page).to have_content(game.game_title)
+    end
+
+    it "ゲームログ詳細が見れる" do
+      visit game_log_path(game_log)
+      expect(page).to have_content(game_log.title)
+      expect(page).to have_content(game_log.spending_amount)
+    end
+
+    it "ゲームログ作成ができない" do
+      visit new_game_log_path
+      expect(current_path).to eq "/users/sign_in"
+      expect(page).to have_content("ログインまたは登録が必要です")
+    end
+
+    it "ゲームログ編集ができない" do
+      visit edit_game_log_path(game_log)
+      expect(current_path).to eq "/users/sign_in"
+      expect(page).to have_content("ログインまたは登録が必要です")
+    end
   end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+  describe "ログイン後" do
+    before do
+      sign_in user
+    end
+
+    context "ゲームログ作成" do
+    end
+  end
 end
